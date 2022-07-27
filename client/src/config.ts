1a86c31e40bb791c2d0efffdf67839956d3106d7
daniel-aasa <oluwagbemigaaasa@gmail.com>
2022-07-27 15:40:31 +0100

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'tvb6x0952c';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-ikqedirn.us.auth0.com',
+  clientId: '1BHiRqsn9js8eUy4REcV0x9eece1niSp',
+  callbackUrl: 'http://localhost:3000/callback'
+};
